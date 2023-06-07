7aadab542f6b0536731cabc9b92a7759e6448fb0
Henry Huynh <henryqhuynh@gmail.com>
2023-06-07 00:00:08 -0700

    completed AI function before bed, will add in Game function and menus later

--- script.js
+++ script.js
@@ -31,7 +31,13 @@ var Ai = {
       return {
          width: 18,
          height: 180,
-         
-      }
+         x: side === 'left' ? 150 : this.canvas.width - 150,
+         y: (this.canvas.height / 2) - 35,
+         score: 0,
+         move: DIRECTION.IDLE,
+         speed: 8
+      };
    }
-}
+};
+
+var Game = {}
