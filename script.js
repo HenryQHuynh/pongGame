327c92e0c2305d0aa800c0752f5334f2ee7173a3
Henry Huynh <henryqhuynh@gmail.com>
2023-06-06 18:14:23 -0700

    Creating Variables for ball and Bot functionality

---
+++ script.js
@@ -0,0 +1,37 @@
+// Global Variables
+var DIRECTION = {
+   IDLE: 0,
+   UP: 1,
+   DOWN: 2,
+   LEFT: 3,
+   RIGHT: 4
+};
+
+var rounds = [5, 5, 3, 3, 2];
+var colors = ['#1abc9c', '#2ecc71', '#3498db', '#8c52ff', '#9b59b6']
+
+// Ball (The cube acts as the pong ball)
+var Ball = {
+   new: function (incrementedSpeed) {
+      return {
+         width: 18,
+         height: 18,
+         x: (this.canvas.width / 2) - 9,
+         y: (this.canvas.height / 2) - 9,
+         moveX: DIRECTION.IDLE,
+         moveY: DIRECTION.IDLE,
+         speed: incrementedSpeed || 7
+      };
+   }
+};
+
+// The AI for Player and Bot
+var Ai = {
+   new: function (side) {
+      return {
+         width: 18,
+         height: 180,
+         
+      }
+   }
+}
