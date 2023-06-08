5599343a4a99b4fd92e3cf7f3211ac285197add8
Henry Huynh <henryqhuynh@gmail.com>
2023-06-08 15:33:31 -0700

    Writing function for Game initialization

--- script.js
+++ script.js
@@ -40,4 +40,30 @@ var Ai = {
    }
 };
 
-var Game = {}
+var Game = {
+   initialize: function () {
+      this.canvas = document.querySelector('canvas');
+      this.context = this.canvas.getContext('2d');
+
+      this.canvas.width = 1400;
+      this.canvas.height = 1000;
+
+      this.canvas.style.width = (this.canvas.width / 2) + 'px';
+      this.canvas.style.height = (this.canvas.height / 2) + 'px';
+
+      this.player = Ai.new.call(this, 'left');
+      this.ai = Ai.new.call(this, 'right');
+      this.ball = Ball.new.call(this);
+
+      this.ai.speed = 5;
+      this.running = this.over = false;
+      this.turn = this.ai;
+      this.timer = this.round = 0;
+      this.color = '#8c52ff';
+
+      Pong.menu();
+      Pong.listen();
+   },
+
+   
+};
